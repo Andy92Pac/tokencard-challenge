5d8a64edb208186f419c97c0bf9af859366d4b25
andy92pac <andy.mpondo@gmail.com>
2019-06-07 14:19:56 +0200

    first commit

---
+++ contracts/Tokencard.sol
@@ -0,0 +1,186 @@
+pragma solidity ^0.5.0;
+
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract Tokencard {
+
+	using SafeMath for uint;
+
+	/**
+     * Event for Token contract creation
+     * @param dailyLimit Initial daily limit
+     * @param addresses Initial whitelisted addresses
+     */
+     event LogTokencardContractCreated(uint dailyLimit, address[] addresses);
+
+    /**
+     * Event for ether transfer
+     * @param to Recipient of the transfer
+     * @param amount Amount transfered to the recipient
+     */
+     event LogTransfer(address indexed to, uint amount);
+
+    /**
+     * Event for daily limit change
+     * @param dailyLimit New daily limit value
+     */
+     event LogDailylimitChange(uint dailyLimit);
+
+    /**
+     * Event for new whitelist addition
+     * @param addresses Addresses added to the whitelist
+     */
+     event LogAddedToWhitelist(address[] addresses);
+
+    /**
+     * Event for new whitelist removal
+     * @param addresses Addresses removed from the whitelist
+     */
+     event LogRemovedFromWhitelist(address[] addresses);
+
+
+     struct Spent {
+     	uint _amount;
+     	uint _lastUpdate;	
+     }
+
+	// Whitelisted addresses 
+	mapping (address => bool) public m_whitelistedAddresses;
+
+	// Daily spent amount
+	mapping (address => Spent) private m_dailySpent;
+
+	// Smart contract owner
+	address public _owner;
+
+	// Daily limit 
+	uint public _dailyLimit;
+
+	// constant number of seconds in a day
+	uint constant DAY_IN_SECONDS = 86400;
+
+	modifier onlyOwner() { 
+		require (_owner == msg.sender); 
+		_; 
+	}
+
+	/**
+     * @dev Constructor, takes dailyLimit and addresses
+     * @param dailyLimit Initial daily limit set by owner at creation
+     * @param addresses Initial whitelisted addresses set by owner at creation
+     */
+     constructor(uint dailyLimit, address[] memory addresses) public {
+     	_owner = msg.sender;
+
+     	_dailyLimit = dailyLimit;
+     	
+     	_addToWhitelist(addresses);
+
+     	emit LogTokencardContractCreated(_dailyLimit, addresses);
+     }
+
+     // @notice Will receive any eth sent to the contract
+     function () external payable {
+     }
+
+	/**
+     * @dev Function to send ethers
+     * @param to The address that will receive ethers
+     * @param amount The amount of ethers to send
+     * @return A boolean that indicates if the operation was successful
+     */
+     function send(address payable to, uint amount) onlyOwner public returns (bool) {
+
+     	require(address(this).balance >= amount, 'Amount exceeds current balance');
+     	
+     	bool whitelisted = m_whitelistedAddresses[to];
+
+     	uint currentDay = _getDay(now);
+
+     	if (currentDay > m_dailySpent[to]._lastUpdate) {
+     		m_dailySpent[to]._lastUpdate = currentDay;
+     		m_dailySpent[to]._amount = 0;
+     	}
+
+     	uint newSpentAmount = m_dailySpent[to]._amount.add(amount);
+
+     	if (whitelisted == false) {
+     		require (newSpentAmount <= _dailyLimit, 'Spent amount exceeds daily limit');
+     	}
+
+     	m_dailySpent[to]._amount = newSpentAmount;
+
+     	to.transfer(amount);
+
+     	emit LogTransfer(to, amount);
+     	return true;
+     }
+
+    /**
+     * @dev Function to set daily limit
+     * @param dailyLimit The new daily limit to be set
+     * @return A boolean that indicates if the operation was successful
+     */
+     function setDailyLimit(uint dailyLimit) onlyOwner public returns (bool) {
+     	require (dailyLimit >= 0);
+
+     	_dailyLimit = dailyLimit;
+
+     	emit LogDailylimitChange(_dailyLimit);
+     	return true;
+     }
+
+    /**
+     * @dev Function to add new addresses to the whitelist
+     * @param addresses The addresses to be whitelisted
+     * @return A boolean that indicates if the operation was successful
+     */
+     function addToWhitelist(address[] memory addresses) onlyOwner public returns (bool) {
+     	require (addresses.length > 0, 'Empty address array');
+
+     	_addToWhitelist(addresses);
+
+     	emit LogAddedToWhitelist(addresses);
+     	return true;
+     }
+
+    /**
+     * @dev Function to remove addresses from the whitelist
+     * @param addresses The addresses to be removed from the whitelist
+     * @return A boolean that indicates if the operation was successful
+     */
+     function removeFromWhitelist(address[] memory addresses) onlyOwner public returns (bool) {
+     	require (addresses.length > 0, 'Empty address array');
+
+     	for(uint i=0; i<addresses.length; i++) {
+     		m_whitelistedAddresses[addresses[i]] = false;
+     	}
+
+     	emit LogRemovedFromWhitelist(addresses);
+     	return true;
+     }
+
+    /**
+     * @dev Internal function to add new addresses to the whitelist, allows to create constructor without any whitelisted address
+     * @param addresses The addresses to be whitelisted
+     * @return A boolean that indicates if the operation was successful
+     */
+     function _addToWhitelist(address[] memory addresses) internal returns (bool) {
+     	for(uint i=0; i<addresses.length; i++) {
+     		m_whitelistedAddresses[addresses[i]] = true;
+     	}
+     	return true;
+     }
+
+    /**
+     * @dev Internal function to get the number of the current day
+     * @param timestamp Timestamp of the date we want to get the day from
+     * @return A uint corresponding to the number of the current day
+     */
+     function _getDay(uint timestamp) internal pure returns (uint) {
+     	return timestamp.div(DAY_IN_SECONDS);
+     }
+ }
+
---
+++ test/index.js
@@ -0,0 +1,160 @@
+const Tokencard = artifacts.require('./../Tokencard.sol');
+const { BN, expectEvent, expectRevert, time } = require('openzeppelin-test-helpers');
+
+contract('Tokencard', ([owner, whitelistedAccount, otherWhitelistedAccount, notWhitelistedAccount, ...accounts]) => {
+
+	let dailyLimit = '1000';
+	let instance = null;
+
+	it('should create contract and log initial dailyLimit and whitelisted addresses', async () => {
+		instance =  await Tokencard.new(dailyLimit, [whitelistedAccount], { from: owner });
+
+		await expectEvent.inConstruction(
+			instance, 
+			'LogTokencardContractCreated');
+	})
+
+	it('should have daily limit of 1000', async () => {
+		let _dailyLimit = await instance._dailyLimit();
+
+		assert.deepEqual(_dailyLimit.toString(), dailyLimit, "Daily limit incorrect");
+	})
+
+	it('should have whitelistedAccount whitelisted', async () => {
+		let isWhitelisted = await instance.m_whitelistedAddresses(whitelistedAccount);
+		
+		assert.deepEqual(isWhitelisted, true, "whitelistedAccount is not whitelisted");
+	})
+
+	it('should change daily limit to 100', async () => {
+		dailyLimit = '100';
+
+		let txReceipt = await instance.setDailyLimit(dailyLimit, { from: owner });
+
+		await expectEvent.inTransaction(
+			txReceipt.tx,
+			Tokencard,
+			'LogDailylimitChange');
+
+		let _dailyLimit = await instance._dailyLimit();
+
+		assert.deepEqual(_dailyLimit.toString(), dailyLimit, "Daily limit incorrect");
+	})
+
+	it('should have otherWhitelistedAccount not whitelisted yet', async () => {
+		let isWhitelisted = await instance.m_whitelistedAddresses(otherWhitelistedAccount);
+		
+		assert.deepEqual(isWhitelisted, false, "otherWhitelistedAccount is whitelisted");
+	})
+
+	it('should add otherWhitelistedAccount to whitelist', async () => {
+		let txReceipt = await instance.addToWhitelist([otherWhitelistedAccount], { from: owner });
+
+		await expectEvent.inTransaction(
+			txReceipt.tx,
+			Tokencard,
+			'LogAddedToWhitelist');
+
+		let isWhitelisted = await instance.m_whitelistedAddresses(otherWhitelistedAccount);
+		
+		assert.deepEqual(isWhitelisted, true, "otherWhitelistedAccount is not whitelisted");
+	})
+
+	it('should remove otherWhitelistedAccount from whitelist', async () => {
+		let txReceipt = await instance.removeFromWhitelist([otherWhitelistedAccount], { from: owner });
+
+		await expectEvent.inTransaction(
+			txReceipt.tx,
+			Tokencard,
+			'LogRemovedFromWhitelist');
+
+		let isWhitelisted = await instance.m_whitelistedAddresses(otherWhitelistedAccount);
+		
+		assert.deepEqual(isWhitelisted, false, "otherWhitelistedAccount is still whitelisted");
+	})
+
+	it('should revert because contract balance is empty', async () => {
+		let amount = '1000'
+		await expectRevert(
+			instance.send(whitelistedAccount, amount, { from: owner }),
+			'Amount exceeds current balance'
+			);
+	})
+
+	it('should send 1 ether to contract', async () => {
+		await web3.eth.sendTransaction({ from: owner, to: instance.address, value: web3.utils.toWei('1', 'ether') });
+
+		let balance = await web3.eth.getBalance(instance.address);
+		
+		assert.deepEqual(balance, web3.utils.toWei('1', 'ether').toString(), "balance is not equal to 1 ether");
+	})
+
+	it('should send 1000 wei to whitelisted recipient', async () => {
+		let amount = new BN('1000');
+		let contractBalance = await web3.eth.getBalance(instance.address);
+		let recipientBalance = await web3.eth.getBalance(whitelistedAccount);
+		
+		let txReceipt = await instance.send(whitelistedAccount, amount, { from: owner })
+
+		await expectEvent.inTransaction(
+			txReceipt.tx,
+			Tokencard,
+			'LogTransfer');
+
+		let newContractBalance = await web3.eth.getBalance(instance.address);
+		let newRecipientBalance = await web3.eth.getBalance(whitelistedAccount);
+
+		assert.deepEqual(newContractBalance.toString(), new BN(contractBalance).sub(amount).toString(), "new contract balance is incorrect");
+		assert.deepEqual(newRecipientBalance.toString(), new BN(recipientBalance).add(amount).toString(), "new recipient balance is incorrect");
+	})
+
+	it('should send 80 wei to not whitelisted recipient', async () => {
+		let amount = new BN('80');
+		let contractBalance = await web3.eth.getBalance(instance.address);
+		let recipientBalance = await web3.eth.getBalance(notWhitelistedAccount);
+		
+		let txReceipt = await instance.send(notWhitelistedAccount, amount, { from: owner })
+
+		await expectEvent.inTransaction(
+			txReceipt.tx,
+			Tokencard,
+			'LogTransfer');
+
+		let newContractBalance = await web3.eth.getBalance(instance.address);
+		let newRecipientBalance = await web3.eth.getBalance(notWhitelistedAccount);
+
+		assert.deepEqual(newContractBalance.toString(), new BN(contractBalance).sub(amount).toString(), "new contract balance is incorrect");
+		assert.deepEqual(newRecipientBalance.toString(), new BN(recipientBalance).add(amount).toString(), "new recipient balance is incorrect");
+	})
+
+	it('should revert because recipient is not whitelisted and amount exceeds daily limit', async () => {
+		let amount = new BN('80');
+		
+		await expectRevert(
+			instance.send(notWhitelistedAccount, amount, { from: owner }),
+			'Spent amount exceeds daily limit'
+			);
+	})
+
+	it('should send 80 wei to not whitelisted recipient because daily limit has been reset', async () => {
+		await time.increase(time.duration.days(1));
+
+		let amount = new BN('80');
+		let contractBalance = await web3.eth.getBalance(instance.address);
+		let recipientBalance = await web3.eth.getBalance(notWhitelistedAccount);
+		
+		let txReceipt = await instance.send(notWhitelistedAccount, amount, { from: owner })
+
+		await expectEvent.inTransaction(
+			txReceipt.tx,
+			Tokencard,
+			'LogTransfer');
+
+		let newContractBalance = await web3.eth.getBalance(instance.address);
+		let newRecipientBalance = await web3.eth.getBalance(notWhitelistedAccount);
+
+		assert.deepEqual(newContractBalance.toString(), new BN(contractBalance).sub(amount).toString(), "new contract balance is incorrect");
+		assert.deepEqual(newRecipientBalance.toString(), new BN(recipientBalance).add(amount).toString(), "new recipient balance is incorrect");
+	})
+
+})
